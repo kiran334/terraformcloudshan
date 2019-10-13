0080f3ab86720004907e3692676128c756669cb2
shan0809 <shantanu_das90@outlook.com>
2019-10-13 21:09:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 resource "azurerm_resource_group" "test" {
-  name     = "shan"
+  name     = "demo"
   location = "West US"
 
   tags = {
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "test" {
   }
 }
 resource "azurerm_storage_account" "test" {
-  name                     = "storageaccountnamedemo12"
+  name                     = "demoforusers"
   resource_group_name      = azurerm_resource_group.test.name
   location                 = azurerm_resource_group.test.location
   account_tier             = "Standard"
