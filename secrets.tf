c5adc27530fad78faa9b9265883535a19f971243
shan0809 <shantanu_das90@outlook.com>
2019-10-13 20:37:01 +0530

    Update secrets.tf

--- secrets.tf
+++ secrets.tf
@@ -1,23 +1,11 @@
-variable "subscription_id" {
-  default = ""
-}
-variable "client_id" {
-  default = ""
-}
 
-variable "client_secret" {
-  default = ""
-}
-variable "tenant_id" {
-  default = ""
-}
 
 provider "azurerm" {
   # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
   version = "=1.34.0"
 
-  subscription_id = "${var.subscription_id}"
-  client_id       = "${var.client_id}"
-  client_secret   = "${var.client_secret}"
-  tenant_id       = "${var.tenant_id}"
+  subscription_id = 
+  client_id       = "22026969-8f12-4da3-ad4d-5ec8fd9d3393"
+  client_secret   = "OJdZehgWfp_dK3xjdyP3o5Vk?@Hq9yH["
+  tenant_id       = "e379ae80-8960-4d12-a7ac-228c4044b133"
 }
