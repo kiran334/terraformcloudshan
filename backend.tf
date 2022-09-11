74aea651fb6ab041e194e7f63699c101f334ed5d
kiran334 <112266740+kiran334@users.noreply.github.com>
2022-09-10 22:11:50 -0400

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "testResourceGroup1"
-    storage_account_name = "storageaccountnamedemo12"
-    container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
-  }
-}
-
