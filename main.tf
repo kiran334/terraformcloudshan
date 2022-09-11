3654a89fc1a68748380d00bc9d3e85f5bb31465e
kiran334 <112266740+kiran334@users.noreply.github.com>
2022-09-10 22:12:14 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,29 @@
-variable "name"{
-  default = ""
-  }
-resource "azurerm_resource_group" "saikirandevrg" {
-  name     = "${var.name}"
-  location = "East US2"
-
-  tags = {
-    environment = "Developmt"
-  }
-}
-resource "azurerm_storage_account" "saikirandevstacct" {
-  name                     = "demoforusers"
-  resource_group_name      = azurerm_resource_group.saikirandevrg.name
-  location                 = azurerm_resource_group.saikirandevrg.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-
-  tags = {
-    environment = "staging"
-    KEY = "VALUE"
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
   }
 }
 
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+}
