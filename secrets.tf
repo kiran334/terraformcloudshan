68dac38571a7c81d71bf98153afc7c40786f227e
kiran334 <112266740+kiran334@users.noreply.github.com>
2022-09-10 22:10:15 -0400

    Update secrets.tf

--- secrets.tf
+++ secrets.tf
@@ -1,8 +1,12 @@
 
-
-provider "azurerm" {
-  # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
-  version = "=3.0.0"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
 
   subscription_id = "ef16b033-ec3e-4659-b1b7-1532f58b43e3"
   client_id       = "dcc35921-6a69-4b62-bd4f-a68324a88844"
